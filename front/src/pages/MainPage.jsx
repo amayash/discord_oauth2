0b20593a7de9e9db0dc260a26e495722424e22c1
dasha <amayashimizu89@gmail.com>
2023-03-07 01:30:39 +0400

    туц ыефпу

--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -1,13 +1,32 @@
 import React, {useEffect, useState} from 'react';
 
-const MainPage = ({ getUser, data }) => {
-
+const MainPage = () => {
+    const [data, setData] = useState();
+    useEffect(() => {
+        fetch("http://localhost:8080/users/@me",{
+                method: 'GET',
+                credentials: 'include',
+            }
+        )
+            .then(r=>r.json())
+            .then(data=>setData(data))
+    }, []);
     return (
-        <div className="d-flex flex-column align-items-start">
-            Хароуш
-                <button onClick={getUser}>Кто я</button>
-            {data}
-        </div>
+        <>
+            {
+                data ?
+                <div className="profile_bg card bg-light mt-5 ms-5">
+                    <div className="d-flex m-2">
+                        <img src={data.avatar} className="rounded my-2"/>
+                        <div className="mt-1 ms-3 fs-2">{data.username}</div>
+                    </div>
+                </div>
+                : null
+            }
+            <div className="text-center">
+                <button className="btn btn-light inner fs-1 border-0 rounded">Создать игру</button>
+            </div>
+        </>
     );
 };
 
--- src/main/java/com/example/demo/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/security/WebSecurityConfig.java
@@ -41,8 +41,10 @@ public class WebSecurityConfig {
                     csrf.csrfTokenRequestHandler(csrfRequestHandler);
 
                 })
-                .logout(logout ->
-                        logout.logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpStatus.OK.value())))
+                .logout(logout -> {
+                        logout.logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpStatus.OK.value()));
+                        logout.logoutSuccessUrl("http://localhost:3000/login");
+                })
                 .oauth2Login(oauth2 -> {
                     oauth2.userInfoEndpoint(user -> user.userService(this.oAuth2UserService));
                     oauth2.defaultSuccessUrl("http://localhost:3000/", true);
